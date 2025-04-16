3b23de284a46653c668d84f60e54467d68ce150d
nbrinson2 <nickbrinson3@gmail.com>
2025-04-16 14:44:35 -0400

    update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { ExampleDisplayComponent } from './example-display/example-display.component';
-import { UnsavedChangesModalComponent } from './unsave-changes-modal/unsave-changes-modal.component';
+import { LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
 import { HighlightJsModule } from 'ngx-highlight-js';
 import { AuthControlsComponent } from './auth-controls/auth-controls.component';
-import { MatCardModule } from '@angular/material/card';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { RouterModule } from '@angular/router';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { LayoutModule } from '@angular/cdk/layout';
+import { ExampleDisplayComponent } from './example-display/example-display.component';
 import { ExampleInfoComponent } from './example-info/example-info.component';
+import { UnsavedChangesModalComponent } from './unsave-changes-modal/unsave-changes-modal.component';
 
 @NgModule({
   declarations: [ExampleDisplayComponent, UnsavedChangesModalComponent, AuthControlsComponent, BreadcrumbsComponent, ExampleInfoComponent],
@@ -30,7 +30,6 @@ import { ExampleInfoComponent } from './example-info/example-info.component';
     ReactiveFormsModule,
     MatDialogModule,
     MatButtonModule,
-    HttpClientModule,
     HighlightJsModule,
     MatSidenavModule,
     MatIconModule,
@@ -48,7 +47,6 @@ import { ExampleInfoComponent } from './example-info/example-info.component';
     ReactiveFormsModule,
     MatDialogModule,
     MatButtonModule,
-    HttpClientModule,
     HighlightJsModule,
     MatSidenavModule,
     MatIconModule,
@@ -63,6 +61,9 @@ import { ExampleInfoComponent } from './example-info/example-info.component';
     UnsavedChangesModalComponent,
     AuthControlsComponent,
     BreadcrumbsComponent,
+  ],
+  providers: [
+    provideHttpClient(),
   ],
 })
 export class SharedModule {}
